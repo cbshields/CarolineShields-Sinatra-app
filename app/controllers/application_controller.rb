e1eb6cbaf20027c8134b857e4e7720746f04a508
Caroline Shields <caroline.shields@amkor.com>
2018-01-29 22:10:18 +0000

    index,layout,login.erb and app_controller.erb

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,20 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/login' do
+    erb :login
+  end
+
+end #ends ApplicationController
